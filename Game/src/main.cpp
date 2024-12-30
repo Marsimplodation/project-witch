ea6275519c8064793874f38c51a8f35f572bccc4
Marius Werkmeister <m.werkmeister@tu-braunschweig.de>
2024-12-30 11:39:18 +0100

    instanceees

--- Game/src/main.cpp
+++ Game/src/main.cpp
@@ -26,20 +26,19 @@ void printFPS(float deltaTime) {
 }
 
 float fov = 45;
-void rotateCamera(float deltaTime) {
+void updateCamera(float deltaTime) {
     engine.mainCamera.projection = glm::perspective(glm::radians(fov), engine.renderingResolution[0] / engine.renderingResolution[1], 0.1f, 10.0f);
     engine.mainCamera.projection[1][1] *= -1;
     engine.mainCamera.view = glm::lookAt(glm::vec3(2.0f, 2.0f, 2.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f)); 
 };
 
-float elapsedTime = 0.0f;
 void rotateAllModels(float deltaTime) {
-    elapsedTime += deltaTime;
     auto view = engine.entities.view<Model>();
     for (auto entity : view) {
         auto& model = view.get<Model>(entity);
-	model.modelMatrices[0] = glm::rotate(glm::mat4(1.0f), elapsedTime * glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
-	model.modelMatrices[1] = glm::rotate(glm::mat4(1.0f), -elapsedTime * glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+	for (auto & matrix : model.modelMatrices) {
+	    matrix = glm::rotate(matrix, deltaTime * glm::radians(90.0f), glm::vec3(0,0,1));
+	}
 
     }
 };
@@ -48,16 +47,17 @@ int main (int argc, char *argv[]) {
     Model quadInfo = {
 	.indexOffset=0,
 	.triangleCount=2,
-	.instanceCount=2,
+	.instanceCount=100,
 	.modelMatrices = {
-	    glm::mat4(1.0f),
-	    glm::translate(glm::mat4(1.0f), glm::vec3(0,0,1)),
 	},
     };
+    for (int i = 0; i < 100; ++i){
+	quadInfo.modelMatrices.push_back(glm::translate(glm::mat4(1.0f), glm::vec3(0,0,1.0f/(100.0f - i))));
+    }
     auto quad = engine.entities.create();
     engine.entities.emplace<Model>(quad, quadInfo);
-    engine.systems.push_back(rotateCamera);
+    engine.systems.push_back(updateCamera);
     engine.systems.push_back(rotateAllModels);
-    //engine.systems.push_back(printFPS);
+    engine.systems.push_back(printFPS);
     engine.run();
 }
