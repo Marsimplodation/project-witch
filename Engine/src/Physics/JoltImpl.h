6025397da31b1d4749e0b28b2a1143b854f855b3
Marius Werkmeister <m.werkmeister@tu-braunschweig.de>
2025-01-10 21:41:11 +0100

    code style stuff

--- Engine/src/Physics/JoltImpl.h
+++ Engine/src/Physics/JoltImpl.h
@@ -15,6 +15,55 @@
 #include <Jolt/Physics/Body/BodyActivationListener.h>
 #include <Jolt/Physics/PhysicsSystem.h>
 #include <Jolt/Math/Vec3.h>
+using namespace JPH;
+using namespace JPH::literals;
+
+
+// Layer that objects can be in, determines which other objects it can collide with
+// Typically you at least want to have 1 layer for moving bodies and 1 layer for static bodies, but you can have more
+// layers if you want. E.g. you could have a layer for high detail collision (which is not used by the physics simulation
+// but only if you do collision testing).
+namespace Layers {
+    static constexpr ObjectLayer NON_MOVING = 0;
+    static constexpr ObjectLayer MOVING = 1;
+    static constexpr ObjectLayer NUM_LAYERS = 2;
+};
+
+// Each broadphase layer results in a separate bounding volume tree in the broad phase. You at least want to have
+// a layer for non-moving and moving objects to avoid having to update a tree full of static objects every frame.
+// You can have a 1-on-1 mapping between object layers and broadphase layers (like in this case) but if you have
+// many object layers you'll be creating many broad phase trees, which is not efficient. If you want to fine tune
+// your broadphase layers define JPH_TRACK_BROADPHASE_STATS and look at the stats reported on the TTY.
+namespace BroadPhaseLayers {
+    static constexpr BroadPhaseLayer NON_MOVING(0);
+    static constexpr BroadPhaseLayer MOVING(1);
+    static constexpr uint NUM_LAYERS(2);
+};
+
+class ObjectLayerPairFilterImpl : public ObjectLayerPairFilter {
+public:
+    virtual bool ShouldCollide(ObjectLayer inObject1, ObjectLayer inObject2) const override;
+};
+
+class BPLayerInterfaceImpl final : public BroadPhaseLayerInterface {
+public:
+    BPLayerInterfaceImpl(); 
+    virtual uint GetNumBroadPhaseLayers() const override;
+    virtual BroadPhaseLayer	GetBroadPhaseLayer(ObjectLayer inLayer) const override; 
+
+    #if defined(JPH_EXTERNAL_PROFILE) || defined(JPH_PROFILE_ENABLED)
+	virtual const char * GetBroadPhaseLayerName(BroadPhaseLayer inLayer) const override; 
+    #endif // JPH_EXTERNAL_PROFILE || JPH_PROFILE_ENABLED
+private:
+    BroadPhaseLayer mObjectToBroadPhase[Layers::NUM_LAYERS];
+};
+
+class ObjectVsBroadPhaseLayerFilterImpl : public ObjectVsBroadPhaseLayerFilter {
+public:
+    virtual bool ShouldCollide(ObjectLayer inLayer1, BroadPhaseLayer inLayer2) const override;
+};
+
+
 
 int testPhysics();
 
